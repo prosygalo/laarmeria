5415bf0c37bb22b935d6b937f8b29b0bd3727942
Rosely <32555309+prosygalo@users.noreply.github.com>
2021-09-15 18:43:00 -0600

    actualizado detalle de compra
    agregar campos al detalle de compra

--- module/Boletacompra/src/Model/DetallecompraTable.php
+++ module/Boletacompra/src/Model/DetallecompraTable.php
@@ -17,35 +17,40 @@ class DetallecompraTable
      }
 
 
-    public function detallecompra($Cod_Boleta)
+    public function detallecompra($Cod_Boleta_Compra)
      {      
        /* SELECT *
         FROM detalle
-        inner join boletas_guia_remision
-        ON detalle.Boleta = boletas_guia_remision.Cod_Boleta inner join productos ON productos.Cod_Producto =  detalle.Producto
-        where Boleta = '29';*/
+        inner join boletas_compra
+        ON detalle_compra.Boleta_Compra = boletas_compra.Cod_Boleta_Compra
+        where Boleta_Compra = '29';*/
                 
                 $sqlSelect = $this->DetallecompraTableGateway->getSql()->select();
-                $sqlSelect->columns(array('Cod_Producto','Cantidad'));
-                $sqlSelect->join('productos', 'productos.Cod_Producto = detalle.Cod_Producto', array('Nombre_Producto'), 'left');
-                $sqlSelect->where(['Cod_Boleta' => $Cod_Boleta]);
+                $sqlSelect->columns(array('Cod_Producto','Nombre_Producto','Descripcion_Producto','Cantidad','Precio'));
+                $sqlSelect->where(['Cod_Boleta_Compra' => $Cod_Boleta_Compra]);
                 $resultSet = $this->DetallecompraTableGateway->selectWith($sqlSelect);
                 return $resultSet;
                 
      }  
 
-    public function insertDetalle($Cod_Producto, $lasId, $Cantidad)
+    public function insertDetalle($Cod_Producto, $Nombre_Producto, $Descripcion_Producto, $Cantidad, $Precio,  $lasId)
     {  
         $Cod_Producto = $Cod_Producto;
+        $Nombre_Producto = $Nombre_Producto;
+        $Descripcion_Producto = $Descripcion_Producto;
+        $Cantidad = $Cantidad;
+        $Precio = $Precio;
         $lasId = $lasId;
-        $Cantidad = $Cantidad; 
 
         for($count = 0; $count < count($Cod_Producto); $count++){
             $data = array();           
              $data=[
                 'Cod_Producto' =>$Cod_Producto[$count],
-                'Cod_Boleta'   =>$lasId,
+                'Nombre_Producto' =>$Nombre_Producto[$count],
+                'Descripcion_Producto' =>$Descripcion_Producto[$count],
                 'Cantidad' =>$Cantidad[$count],
+                'Cod_Producto' =>$Cod_Producto[$count],
+                'Cod_Boleta'   =>$lasId,
             ];
           
           $this->DetallecompraTableGateway->insert($data);
